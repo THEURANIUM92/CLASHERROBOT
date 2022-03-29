959a0bc1f5d3704d9c88432201049392117a34fe
KEN KANEKI <81973202+kenkanekiop@users.noreply.github.com>
2022-03-29 12:47:39 +0530

    Update logomaker.py

--- SiestaRobot/modules/logomaker.py
+++ SiestaRobot/modules/logomaker.py
@@ -114,7 +114,7 @@ async def logo_gen(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [Siesta Robot](https://t.me/Siestaxbot)",
+            caption="Logo by [Uranium Robot](https://t.me/URANIUM_MUSIC_BOT)",
             force_document=False,
         )
         os.remove(flnme)
@@ -197,7 +197,7 @@ async def logo_(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [Siesta Robot](https://t.me/Siestaxbot)",
+            caption="Logo by [Uranium Robot](https://t.me/URANIUM_MUSIC_BOT)",
             force_document=False,
         )
         os.remove(flnme)
